ce4c69b36d6e3f0f7085f22fc20c0c167abc0a75
Spyke Matthew Lim <matty.lim718@gmail.com>
2024-10-21 17:33:38 +0800

    Added GUI

---
+++ src/main/java/org/example/CrudInventory.java
@@ -0,0 +1,77 @@
+package org.example;
+
+import java.sql.*;
+
+public class CrudInventory {
+    private static final String DB_URL = "jdbc:sqlite:C:/Users/c202301028/IdeaProjects/MavenProject/spykeDatabase.db";
+
+    public void addInventory(int quantity,int mealID) {
+        String insertSQL = "INSERT INTO Inventory (quantity, meal_id) VALUES (?, ?)";
+        try (Connection connection = DriverManager.getConnection(DB_URL);
+             PreparedStatement preparedStatement = connection.prepareStatement(insertSQL)) {
+            preparedStatement.setInt(1, quantity);
+            preparedStatement.setInt(2, mealID);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void editInventory(int inventory_id, int meal_id, int quantity) {
+        String updateSQL = "UPDATE \"Inventory\" SET meal_id = ?, quantity = ? WHERE inventory_id = ?";
+
+        try (Connection connection = DriverManager.getConnection(DB_URL);
+             PreparedStatement preparedStatement = connection.prepareStatement(updateSQL)) {
+
+            preparedStatement.setInt(1, meal_id);
+            preparedStatement.setInt(2, quantity);
+            preparedStatement.setInt(3, inventory_id);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Inventory updated successfully!");
+            } else {
+                System.out.println("No inventory found with the provided ID.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteInventory(int inventory_id) {
+        String deleteSQL = "DELETE FROM Inventory WHERE id = ?";
+
+        try (Connection connection = DriverManager.getConnection(DB_URL);
+             PreparedStatement preparedStatement = connection.prepareStatement(deleteSQL)) {
+
+            preparedStatement.setInt(1, inventory_id);
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Inventory entry for meal ID " + inventory_id + " deleted successfully!");
+            } else {
+                System.out.println("No inventory entry found for meal ID " + inventory_id);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void listInventory() throws SQLException {
+        String query = "SELECT * FROM Inventory";
+        StringBuilder inventorylist = new StringBuilder("Inventory:\n");
+        try (Connection connection = DriverManager.getConnection(DB_URL);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+
+            while (resultSet.next()) {
+                inventorylist.append("ID: ").append(resultSet.getInt("id"))
+                        .append("\n Quantity: ").append(resultSet.getString("quantity"))
+                        .append("\n Meal ID: ").append(resultSet.getString("meal_id"))
+                        .append("\n");
+
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/CrudMeal.java
@@ -0,0 +1,106 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CrudMeal {
+
+    private static final String DB_URL = "jdbc:sqlite:C:/Users/c202301028/IdeaProjects/MavenProject/spykeDatabase.db";
+
+    public void addMeal(String mealName, String category,int servingSize, String type,int nutritionalValue, String
+            spice,double mealPrice, String ingredients) {
+        String insertSQL = "INSERT INTO Meals (Meal Name, Meal Category, Serving Size, Meal Type, Nutritional Value, Spice Level, Meal Price, Ingredients) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        try (Connection connection = DriverManager.getConnection(DB_URL);
+             PreparedStatement preparedStatement = connection.prepareStatement(insertSQL)) {
+            preparedStatement.setString(1, mealName);
+            preparedStatement.setString(2, category);
+            preparedStatement.setInt(3, servingSize);
+            preparedStatement.setString(4, type);
+            preparedStatement.setInt(5, nutritionalValue);
+            preparedStatement.setString(6, spice);
+            preparedStatement.setDouble(7, mealPrice);
+            preparedStatement.setString(8, ingredients);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void editMeal(int meal_id, String meal_name, String meal_category,
+                                 String meal_type, String nutritional_value,
+                                 String spicy_or_not_spicy, float meal_price,
+                                 String ingredients) {
+        String updateSQL = "UPDATE Meals SET meal_name = ?, meal_category = ?, meal_type = ?, " +
+                "nutritional_value = ?, spicy_or_not_spicy = ?, meal_price = ?, ingredients = ? " +
+                "WHERE meal_id = ?";
+
+        try (Connection connection = DriverManager.getConnection(DB_URL);
+             PreparedStatement preparedStatement = connection.prepareStatement(updateSQL)) {
+
+            preparedStatement.setString(1, meal_name);
+            preparedStatement.setString(2, meal_category);
+            preparedStatement.setString(3, meal_type);
+            preparedStatement.setString(4, nutritional_value);
+            preparedStatement.setString(5, spicy_or_not_spicy);
+            preparedStatement.setFloat(6, meal_price);
+            preparedStatement.setString(7, ingredients);
+            preparedStatement.setInt(8, meal_id); // Use the provided meal ID
+
+            int rowsAffected = preparedStatement.executeUpdate(); // Execute the update
+            if (rowsAffected > 0) {
+                System.out.println("Meal updated successfully!");
+            } else {
+                System.out.println("No meal found with the provided ID.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteMeal(int meal_id) {
+        String deleteSQL = "DELETE FROM Meals WHERE meal_id = ?";
+
+        try (Connection connection = DriverManager.getConnection(DB_URL);
+             PreparedStatement preparedStatement = connection.prepareStatement(deleteSQL)) {
+
+            preparedStatement.setInt(1, meal_id);
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Meal with ID " + meal_id + " deleted successfully.");
+            } else {
+                System.out.println("No meal found with ID " + meal_id);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void listMeals() throws SQLException {
+        String query = "SELECT * FROM Meals";
+        StringBuilder mealList = new StringBuilder("Meals:\n");
+        try (Connection connection = DriverManager.getConnection(DB_URL);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+
+            while (resultSet.next()) {
+                mealList.append("ID: ").append(resultSet.getInt("meal_id"))
+                        .append("\n Meal Name: ").append(resultSet.getString("meal_name"))
+                        .append("\n Meal Category: ").append(resultSet.getString("meal_category"))
+                        .append("\n Serving Size: ").append(resultSet.getString("meal_type"))
+                        .append("\n Meal Type: ").append(resultSet.getString("meal_type"))
+                        .append("\n Nutritional Value: ").append(resultSet.getString("nutritional_value"))
+                        .append("\n Spicy: ").append(resultSet.getString("spicy_or_not_spicy"))
+                        .append("\n Meal Price: ").append(resultSet.getString("meal_price"))
+                        .append("\n Ingredients: ").append(resultSet.getString("ingredients")).append("\n");
+
+            }
+        }
+    }
+}
